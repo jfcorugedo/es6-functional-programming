c837a2ac16cb228874fc794200c531e4e3b971f1
jfcorugedo <baronrojo.github>
2018-04-10 16:43:37 +0200

    version ready for staring development

--- test/execisesTest.js
+++ test/execisesTest.js
@@ -8,11 +8,14 @@ const expect = chai.expect;
 
 describe('exercises', () => {
 
+    const ___ = 'unimplemented';
+
     describe('basics (all the data passed to functions comes from data.js, unless explicitly stated on the test)', () => {
         // suggestion: use currify and map
         it('return a new array greeting all names', () => {
 
-            const greetAll = (names) => names.map(name => `hi ${name}`);
+            /* TODO */
+            const greetAll = () => ___;
 
             expect(greetAll(names)).to.deep.equal(['hi juan', 'hi ivan', 'hi jose', 'hi sebas', 'hi miguel', 'hi ricardo', 'hi edu']);
         });
@@ -20,7 +23,8 @@ describe('exercises', () => {
         // suggestion: use reduce
         it('return the sum of the numbers array', () => {
 
-            const getSum = (numbers) => numbers.reduce((sum, number) => sum + number);
+            /* TODO */
+            const getSum = () => ___;
 
             expect(getSum(numbers)).to.deep.equal(150);
         });
@@ -28,16 +32,15 @@ describe('exercises', () => {
         // suggestion: use reduce
         it('return the greatest number in the array', () => {
 
-            const getGreatest = numbers => numbers.reduce((greatest, number) => greatest > number ? greatest : number);
+            /* TODO */
+            const getGreatest = () => ___;
             expect(getGreatest(numbers)).to.deep.equal(60);
         });
 
         // restriction: use compose
         it('return the area of a triangle', () => {
-
-            const multiply = (triangle) => triangle.base*triangle.height;
-            const half = c => c/2;
-            const triangleArea = compose(half, multiply);
+            /* TODO */
+            const triangleArea = () => ___;
 
             expect(triangleArea(triangle)).to.deep.equal(50);
         });
@@ -48,7 +51,8 @@ describe('exercises', () => {
         describe('Given a collection of writters (in data.js), correctIncomes function', () => {
             it ('should correct a typo in their incomes that returns a new array with only the incomes corrected multiplied by 1000', () => {
 
-                const correctIncomes = (writters) => writters.map(writter => writter.incomes * 1000);
+                /* TODO */
+                const correctIncomes = () => ___;
 
                 expect(correctIncomes(writters)).to.deep.equal([ 93000, 44000, 98000, 13000 ]);
             });
@@ -59,10 +63,8 @@ describe('exercises', () => {
     
             it('should return an array with numbers from 1 to 30 replacing multiples of 3 by fizz, multiples of 5 by buzz and multiples of both by fizzbuzz', () => {
 
-                const fizz = (number) => number%3 === 0 ? 'fizz' : number;
-                const buzz = (number) => number%5 === 0 ? 'buzz' : number;
-                const fizzbuzz = (number) => number%3 === 0 && number%5 === 0 ? 'fizzbuzz' : number;
-                const functionalFizzBuzz = () => [...Array(31).keys()].slice(1).map(compose(fizz, buzz, fizzbuzz));
+                /* TODO */
+                const functionalFizzBuzz = () => ___;
 
                 expect(functionalFizzBuzz()).to.deep.equal(result);
 
@@ -78,9 +80,8 @@ describe('exercises', () => {
             // ];
             const bookPrices = [32, 59, 72, 75];
 
-            const toPrices = (books) => books.map(book => book.price);
-            const sort = (prices) => prices.sort((a, b) => a > b);
-            const sortList = compose(sort, toPrices);
+            /* TODO */
+            const sortList = () => ___;
             expect(sortList(books)).to.deep.equal(bookPrices);
         });
     });
